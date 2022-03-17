aa7a4c7271cc6e449427b741cb6f3e771241a1e4
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-03-17 14:29:13 +0000

    completed data, added routes, created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import './App.scss';
+import users from "./data/data";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import UserProfile from './Components/UserProfile/UserProfile';
+import { useState } from 'react/cjs/react.production.min';
+import NavBar from './Components/NavBar/NavBar';
+import UserList from './Components/UserList/UserList';
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState();
+
+  const handleInput = (e) => {
+    setSearchTerm(e.target.value);
+  }
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router> 
+      <NavBar searchTerm={searchTerm} handleInput={handleInput}/>
+      <Routes>
+        <Route path="/" element={<UserList searchTerm={searchTerm}/>}/>
+        <Route path="/:name" element={<UserProfile users={users}/>}/>
+      </Routes>
+    </Router>
   );
 }
 
--- src/data/data.js
+++ src/data/data.js
@@ -1,51 +1,55 @@
 const users = [
     {
         "name": "khabib_nurmagomedov",
-        "profile_pic": "", 
+        "profile_pic": "./images/Khabib-Nurmagomedov-profile.webp", 
         "bio": {
+            "title": "",
+            "sub-title": "",
             "text": "Хабиб Нурмагомедов",
             "link": "goflx.com"
         },
         "highlight_1": {
-            "img": "",
-            "title": ""
+            "img": "./images/khabib_highlight_1.webp",
+            "title": "Fights"
         },
         "highlight_2": {
-            "img": "",
-            "title": ""
+            "img": "./images/khabib_highlight_2.webp",
+            "title": "Food"
         }, 
         "posts": "4,439",
         "followers": "33.3m",
         "following": "483",
         "images": {
-            "img_1": "",
-            "img_2": "",
-            "img_3": ""
+            "img_1": "./images/khabib-nurmagomedov-ufc-254-presser-belt.webp",
+            "img_2": "./images/khabib-weighin.jpg",
+            "img_3": "./images/0_Untitled-3.png"
         }
     },
 
     {
         "name": "islam_makhachev",
-        "profile_pic": "", 
+        "profile_pic": "./images/islam_profile.jpg", 
         "bio": {
-            "text": "Хабиб Нурмагомедов",
-            "link": "goflx.com"
+            "title": "Islam_Makhachev",
+            "sub-title": "Sportsperson",
+            "text": "Стремитесь же опередить друг друга в добрых делах (2:148) Сотрудничество. MAKHACHEVMMA@mail.ru",
+            "link": "go.legionfarm.com/islam"
         },
         "highlight_1": {
-            "img": "",
-            "title": ""
+            "img": "./images/islam_family.webp",
+            "title": "Family"
         },
         "highlight_2": {
-            "img": "",
-            "title": ""
+            "img": "./images/islam_training.png",
+            "title": "Training"
         }, 
         "posts": "1,825",
         "followers": "3.8m",
         "following": "917",
         "images": {
-            "img_1": "",
-            "img_2": "",
-            "img_3": ""
+            "img_1": "./images/islam_khabib.avif",
+            "img_2": "./images/islam_win.webp",
+            "img_3": "./images/islam_bobby.webp"
         }
     }
 ]
