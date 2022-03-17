31243f0231b2c5eeea030e5a092efc8f4fb7a9d4
Abdullah Abdulsattar <abdullah_a_s@hotmail.co.uk>
2022-03-17 12:34:00 +0000

    updated mock data

---
+++ src/data/data.js
@@ -0,0 +1,51 @@
+const users = [
+    {
+        "name": "khabib_nurmagomedov",
+        "profile_pic": "", 
+        "bio": {
+            "text": "Хабиб Нурмагомедов",
+            "link": "goflx.com"
+        },
+        "highlight_1": {
+            "img": "",
+            "title": ""
+        },
+        "highlight_2": {
+            "img": "",
+            "title": ""
+        }, 
+        "posts": "4,439",
+        "followers": "33.3m",
+        "following": "483",
+        "images": {
+            "img_1": "",
+            "img_2": "",
+            "img_3": ""
+        }
+    },
+
+    {
+        "name": "islam_makhachev",
+        "profile_pic": "", 
+        "bio": {
+            "text": "Хабиб Нурмагомедов",
+            "link": "goflx.com"
+        },
+        "highlight_1": {
+            "img": "",
+            "title": ""
+        },
+        "highlight_2": {
+            "img": "",
+            "title": ""
+        }, 
+        "posts": "1,825",
+        "followers": "3.8m",
+        "following": "917",
+        "images": {
+            "img_1": "",
+            "img_2": "",
+            "img_3": ""
+        }
+    }
+]
